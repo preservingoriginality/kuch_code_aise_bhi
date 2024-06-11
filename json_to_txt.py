8c4745ae06c1f4d2f974ef686095bf59136bd60b
preservingoriginality <167908457+preservingoriginality@users.noreply.github.com>
2024-06-11 09:13:28 +0530

    Update json_to_txt.py

--- json_to_txt.py
+++ json_to_txt.py
@@ -1,6 +1,6 @@
 import gzip
 import shutil
-with gzip.open('/home/llmmarathi/Pendse/data-00000-of-00019.arrow', 'rb') as fin:
+with gzip.open('/home/llmmarathi/Pendse/data-00000-of-00019.gzip', 'rb') as fin:
     with open('/mnt/HDFS1/llm/llmmarathi/Pendse/glot500/g1.txt', 'wb') as fout:
         #for small files this also works
         # data = fin.read()
